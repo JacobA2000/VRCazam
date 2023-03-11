d2fef30e1f5ed903471b87e307b8864577d1c5c0
Jacob Allen <jacoballen785@gmail.com>
2023-03-11 18:03:20 +0000

    Added both windows and xso overlay notifications

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import soundcard as sc
 import soundfile as sf
 import io
 from pythonosc import dispatcher, osc_server
+import socket
+import json
+from win10toast import ToastNotifier
 
 SAMPLE_RATE = 48000              # [Hz]. sampling rate.
 RECORD_SEC = 10                  # [sec]. duration recording audio.
+NOTIFICATION_METHOD = 0          # method for sending notifications, 0=windows 1=xso
 
 def RecordAudioBytes(sr, rs):
     print("Recording...")
@@ -32,21 +36,60 @@ def RecognizeSong(audio_bytes):
             
             if shazam_data[1]["track"] != "":
                 track_data = shazam_data[1]["track"]
-                print(f"{track_data['title']} - {track_data['subtitle']}")
-                break
+                track_msg = f"{track_data['title']} - {track_data['subtitle']}"
+                print(track_msg)
+                return (track_msg, "Song Recognized")
 
         except:
             StopIteration
             print("Couldn't identify song!")
-            break
+            return ("Shazam couldn't identify the song, please try again.", "Couldn't identify song!")
 
 def SongSearchInit(address, *args):
 
     if args[0] == True:
         print(f"OSC Message Recieved on address {address}.")
         audio_bytes = RecordAudioBytes(SAMPLE_RATE, RECORD_SEC)
-        RecognizeSong(audio_bytes)
+        track_msg = RecognizeSong(audio_bytes)
 
+        SendNotification(content=track_msg[0], msg_type=track_msg[1])
+
+def SendNotification(content, msg_type):
+
+    match NOTIFICATION_METHOD:
+        case 0:
+            toaster = ToastNotifier()
+            toaster.show_toast(f"VRCazam - {msg_type}", content)
+
+        case 1:
+            ip = "127.0.0.1"
+            port = 42069
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+            msg = {
+                "messageType": 1,
+                "index": 0,
+                "title": f"VRCazam - {msg_type}",
+                "content": content,
+                "height": 175.0,
+                "sourceApp": "VRCazam",
+                "timeout": 6.0,
+                "volume": 0.7,
+                "audioPath": "default",
+                "useBase64Icon": False,
+                "icon": "default",
+                "opacity": 1.0
+            }
+
+            msgdata = json.dumps(msg)
+            byte = msgdata.encode()
+
+            sock.sendto(byte, (ip, port))
+
+            sock.close()
+    
 # Set up the dispatcher to route messages to the function
 dispatcher = dispatcher.Dispatcher()
 dispatcher.map("/avatar/parameters/SongSearch", SongSearchInit)
